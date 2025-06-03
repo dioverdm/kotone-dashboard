cdff2a169256ded7e027ecc8ad52525697b3d41c
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-06-02 20:59:32 -0400

    Actualizar Login.js

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -19,7 +19,7 @@ export const Login = () => {
       <div className="bg-gray-800 p-12 rounded-2xl flex flex-col items-center">
         <h2 className="dark:text-white text-5xl text-center mb-4 font-semibold">Discord Login</h2>
         <p className="dark:text-white text-center mb-8">To continue, you need to login with your Discord account</p>
-        <a className="bg-indigo-600 p-4 text-white rounded-2xl font-semibold" href="https://discord.com/oauth2/authorize?client_id=889540062956634164&response_type=code&redirect_uri=https%3A%2F%2Fapi.kotone.tech%2Foauth%2Fcallback&scope=identify+guilds+guilds.join">Login with Discord</a>
+        <a className="bg-indigo-600 p-4 text-white rounded-2xl font-semibold" href="https://discord.com/oauth2/authorize?client_id=814494780251045928&response_type=code&redirect_uri=https%3A%2F%2Fpodash.vercel.app%2Foauth%2Fcallback&scope=identify+guilds+guilds.join">Login with Discord</a>
       </div>
     </div>
   );
